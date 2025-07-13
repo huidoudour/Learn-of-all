780b35caebc957867a7e3141ac7f4206dddbfe2c
huidoudour <ylmf_qquser@126.com>
2025-07-13 19:15:48 +0800

    更新笔记

--- LinBash/NetSystemShell.sh
+++ LinBash/NetSystemShell.sh
@@ -68,8 +68,76 @@ vim /etc/bind/db.root
 systemctl enable --now bind9
 #IspSrv-Web(Nginx+PHP/FastCGI)
 apt -y install nginx php7.3-fpm php7.3 php7.3-cli #如无软件包需要联网下载
-
+mkdir -p /mut/crypt
 echo " <?php echo "Welcome to 2025 Computer Network Application contest"; ?>" > /mut/crypt/index.php
+vim /etc/nginx/conf.d/ispweb.conf
+	server {
+        listen 80;
+        root /mut/crypt;
+        index index.php ;
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php7.3-fpm.sock;
+        }
+	}           
+    //如果让人编写  很头疼
+
+vim /etc/nginx/sites-enabled/default
+	//21-71行 复制到ispweb.conf里面 我们自行修改
+	：21，71y   //进行复制21-71
+
+rm sites-enabled/default  #此时需要删除默认的default文件，不然冲突
+systemctl enable --now nginx php7.3-fpm
+systemctl restart nginx php7.3-fpm
+#end IspSrv
+#RouterSrv-DHCP-relay
+yum -y install dhcp
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+dhcrelay 192.168.100.100
+vim /etc/sysconfig/dhcrelay
+	INTERFACES="ens36" #链接客户端的接口
+	DHCPSERVERS="192.168.100.100"
+systemctl start dhcrelay
+systemctl enable dhcrelay
+#AppSrv-DHCP
+vim /etc/dhcp/dhcpd.conf
+	# DHCP Server Configuration file.
+	# > see /usr/share/doc/dhcp*/dhcpd.conf.example <
+	#   see dhcpd.conf(5) man page
+	#
+	subnet 192.168.100.0 netmask 255.255.255.0 {}
+	subnet 192.168.0.0 netmask 255.255.255.0 {
+	  range 192.168.0.110 192.168.0.190;
+	  option domain-name-servers 192.168.100.100;
+	  option domain-name "chinaskills.cn";
+	  option routers 192.168.0.254;
+	  default-lease-time 600;
+	  max-lease-time 7200;
+	}
+	host insidecli {
+	  hardware ethernet 00:0C:29:4B:5C:FE;
+	  fixed-address 192.168.0.190;
+	}
+#AppSrv-DNS
+yum -y install bind bind-utils
+view in {
+match-clients { localhost;192.168.100.0/24;192.168.0.0/24; };
+	zone "" IN {
+	        type master;
+	        file "named.in";
+	};
+include "/etc/named.rfc1912.zones";
+};
+include "/etc/named.root.key";
+
+view out {
+match-clients { any; };
+	zone "" IN {
+	        type master;
+	        file "named.out";
+	};
+};
 
 
 
